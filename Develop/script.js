a26514c5225e5a71a4290a7d55759f35c623f940
Tina Humphrey <tinahumphrey@Tinas-MacBook-Air.local>
2023-11-20 22:31:51 -0500

    commit javascript and README final draft changes to project

--- Develop/script.js
+++ Develop/script.js
@@ -2,63 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  var myDay = [
-    {
-      id: "0",
-      time: "09",
-      meridam: "am",
-      memo: "",
-    },
-    {
-      id: "1",
-      time: "10",
-      meridam: "am",
-      memo: "",
-    },
-    {
-      id: "2",
-      time: "11",
-      meridam: "am",
-      memo: "",
-    },
-    {
-      id: "3",
-      time: "12",
-      meridam: "pm",
-      memo: "",
-    },
-    {
-      id: "4",
-      time: "01",
-      meridam: "pm",
-      memo: "",
-    },
-    {
-      id: "5",
-      time: "02",
-      meridam: "pm",
-      memo: "",
-    },
-    {
-      id: "6",
-      time: "03",
-      meridam: "pm",
-      memo: "",
-    },
-    {
-      id: "7",
-      time: "04",
-      meridam: "pm",
-      memo: "",
-    },
-    {
-      id: "8",
-      time: "05",
-      meridam: "pm",
-      memo: "",
-    }
-  ]
-
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -71,89 +14,42 @@ $(function () {
     var time = $(this).parent().attr("id")
 
     localStorage.setItem(time, memo)
-})
+  })
   
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?//
-  var currentHour = dayjs().format('hh:mm:ss');
-  var hour = document.getElementById("hour-10");
-  var hour = document.getElementById("hour-11");
-  var hour = document.getElementById("hour-12");
-  var hour = document.getElementById("hour-1");
-  var hour = document.getElementById("hour-2");
-  var hour = document.getElementById("hour-3");
-  var hour = document.getElementById("hour-4");
-  var hour = document.getElementById("hour-5");
-
-  if (myDay > currentHour) {
-    var hour = document.getElementById("hour-9");
-    hour.classList.remove("id");
-  }
-else if(myDay > currentHour)  {
-  var hour = document.getElementById("hour-10");
-  hour.classList.remove("id");
-}
-
-else if 
-(myDay > currentHour) {
-  var hour = document.getElementById("hour-11");
-  hour.classList.remove("id");
-}
-
-else if 
-(myDay > currentHour) {
-  var hour = document.getElementById("hour-12");
-  hour.classList.remove("id");
-}
-
-else if 
-(myDay > currentHour) {
-  var hour = document.getElementById("hour-01");
-  hour.classList.remove("id");
-}
-
-else if 
-(myDay > currentHour) {
-  var hour = document.getElementById("hour-02");
-  hour.classList.remove("id");
-}
-
-else if 
-(myDay > currentHour) {
-  var hour = document.getElementById("hour-03");
-  hour.classList.remove("id");
-}
-
-else if 
-(myDay > currentHour) {
-  var hour = document.getElementById("hour-04");
-  hour.classList.remove("id");
-}
-
-else if 
-(myDay > currentHour) {
-  var hour = document.getElementById("hour-05");
-  hour.classList.remove("id");
-}
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?//
-  var memo = document.querySelector("#memo");
-  localStorage.setItem("memo", memo);
+  function timeCheck (){
+    var currentHour = dayjs().format('HH');
  
-function saveMemo() {
-  localStorage.setItem("myDay", JSON.stringify(myDay));
-}
+    $(".time-block").each(function(){
+      var thisHour = parseInt($(this).attr('id')); 
+  
+  // TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?//
+      $(this).children('.description').val(localStorage.getItem($(this).attr('id')))
+  
+      if (currentHour > thisHour) {
+        $(this).addClass('past')
+      } else if (currentHour === thisHour) {
+        $(this).addClass('present')
+      } else {
+      $(this).addClass('future')
+      }
+    })
+  }
+ 
+
   // TODO: Add code to display the current date in the header of the page.
   setInterval(function getHeaderDate(){
   const currentDay = dayjs();
   const formattedDate = currentDay.format('dddd,MMMM D,YYYY h:mm:ss');
   $('#currentDay').text(formattedDate);
-  
-},1000);
+  },1000);
 
-
+timeCheck();
+mySchedule();
 });
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Work-Day-Scheduler
 
 ## Description 
-
 Modified starter code to create a simple calendar application that allows a user to save events for each hour of a typical working day (9am to 5pm) by. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Installation
+N/A
+
+## Usage 
+Type notes for each time block and save them to schedule your work day. See the Console to view your save notes in the local storage. The save button will also keep your notes displayed on the page when you refresh the page until you enter a new note.
+
+## Credits 
+Used 
+
+## License
+Please refer to the LICENSE in the repo.
+
+## Live Website 
